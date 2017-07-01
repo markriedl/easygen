a084bf72fa133c0c768da87b7dcf799c28f3bd4d
markriedl <mark.riedl@gmail.com>
2017-06-30 20:32:27 -0400

    Made the json output from editor a bit better. Added datasets

--- easygen.py
+++ easygen.py
@@ -8,7 +8,13 @@ parser = argparse.ArgumentParser(description='Run an easygen program.')
 parser.add_argument('program', help="the file containing the easgen program.")
 args = parser.parse_args()
 
-def runModule(module, rest):
+def runModule(module_json):
+	module_json_copy = copy.deepcopy(module_json)
+	module = module_json_copy['module']
+	del module_json_copy['module']
+
+	rest = str(module_json_copy)
+
 	params = rest.replace('{', '').replace('}', '')
 	#params = re.sub(r'u\'', '', params)
 	#params = re.sub(r'\'', '', params)
@@ -24,13 +30,18 @@ def runModule(module, rest):
 	module = eval(evalString)
 	module.run()
 
-data = []
+
+
+### Read in the program file
+data_text = ''
+data = None
 for line in open(args.program, "r"):
-	data.append(json.loads(line))
+	data_text = data_text + line.strip()
 
-for d in data:
-	module = d['module']
-	d2 = copy.deepcopy(d)
-	del d2['module']
-	runModule(module, str(d2))
+### Convert to json dictionary
+data = json.loads(data_text)
 
+### Run each module
+if data is not None:
+	for d in data:
+		runModule(d)
